0ddcf89a4a8ce4565a653a04a88c5cf07b4ac6c4
Acaureliano <acaureliano@gmail.com>
2023-05-25 00:39:50 -0300

    25-05-2023 00:39

--- src/Home.styles.ts
+++ src/Home.styles.ts
@@ -3,31 +3,29 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-
-  background-color: #fff;
+  background-color: #F0F2F5;
 `;
 
-export const StatusBar = styled.StatusBar``;
+export const StatusBar = styled.StatusBar`
+
+`;
 
 export const HeaderBackground = styled.View`
-  flex: 0.2;
+  height: 204px;
   width: 100%;
-
   padding-top: ${Platform.OS === 'android' ? 32 : 62}px;
-
   background-color: #5429cc;
 `;
 
 export const Header = styled.View`
   flex-direction: row;
-
   align-items: center;
   justify-content: space-between;
-
   padding: 0 24px;
 `;
 
-export const Icon = styled.Image``;
+export const Icon = styled.Image`
+`;
 
 export const Title = styled.Text`
   color: #fff;
@@ -35,7 +33,6 @@ export const Title = styled.Text`
   font-weight: 700;
   line-height: 22px;
   text-align: left;
-
   margin-left: 13px;
 `;
 
@@ -48,13 +45,10 @@ export const Logo = styled.View`
 export const ButtonTransition = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
-
   /* width: 130px; */
   height: 40px;
-
   padding: 11px 16px;
   border-radius: 5px;
-
   background-color: #6933ff;
 `;
 
@@ -64,4 +58,27 @@ export const Label = styled.Text`
   font-weight: 500;
   line-height: 22px;
   text-align: left;
+`;
+
+export const CardsContainer = styled.View`
+  height: 200px;
+  background-color: black;
+`;
+
+export const Card01 = styled.View`
+  height: 200px;
+  background-color: red;
+  margin: 0px 20px 0px 20px;
+`;
+
+export const Card02 = styled.View`
+  height: 200px;
+  background-color: blue;
+  margin: 0px 20px 0px 20px;
+`;
+
+export const Card03 = styled.View`
+  height: 200px;
+  background-color: green;
+  margin: 0px 20px 0px 20px;
 `;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,14 @@
-import {
-  ButtonTransition,
-  Label,
-  Container,
-  HeaderBackground,
-  Header,
-  Icon,
-  Title,
-  Logo,
-  StatusBar,
-} from './Home.styles';
+import { ScrollView, View, Image, Dimensions, Text, TouchableOpacity} from 'react-native';
+import {ButtonTransition, Label, Container, CardsContainer, HeaderBackground, Header, Icon, Title, Logo, StatusBar, Card01, Card02, Card03,} from './Home.styles';
 
 const icon = require('./assets/icon-logo.png');
+const {width} = Dimensions.get("window");
+
+const cards = [
+  { id: 1, backgroundColor: '#ffffff', marginRight: 0 },
+  { id: 2, backgroundColor: '#ffffff', marginRight: 0 },
+  { id: 4, backgroundColor: '#33CC95', marginRight: 20 },
+]
 
 export function Home() {
   console.log(StatusBar.currentHeight);
@@ -30,6 +28,30 @@ export function Home() {
             </ButtonTransition>
           </Header>
         </HeaderBackground>
+        <View style={{height: 200, top: -100,}}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} decelerationRate="fast" snapToInterval={351} style={{flexDirection: 'row',}}>
+            {
+              cards.map((item) => (
+                <View key={item.id} style={{width: 330, height: 200, marginRight: item.marginRight, backgroundColor: item.backgroundColor, marginLeft: 20, borderRadius: 8}}>
+                  <View style={{flexDirection: 'row',}}>
+                    <Text style={{flex: 1, marginLeft: 23, marginTop: 23, fontSize: 14, fontWeight: 400, lineHeight: 21 }}>
+                      Entradas
+                    </Text>
+                    <TouchableOpacity>
+                      <Image style={{margin: 23}} source={require('./assets/entradas.png')}/>
+                    </TouchableOpacity>
+                  </View>
+                  <Text style={{flex: 1, marginLeft: 23, marginTop: 15, fontSize: 30, fontWeight: 400,}}>
+                    R$ 17.400,00
+                  </Text>
+                  <Text style={{flex: 1, marginLeft: 23,fontSize: 12, fontWeight: 400}}>
+                    Última entrada dia 13 de abril
+                  </Text>
+                </View>
+              ))
+            }
+          </ScrollView>
+        </View>
       </Container>
     </>
   );
